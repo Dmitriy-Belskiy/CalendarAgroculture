8af6fcf346cb18e49c35d07067976ac8dd2564ff
BDima <diesel@MacBook-Air-BDima.local>
2022-12-11 15:49:12 +0300

    progect Calendare

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Profile from "./Profile";
+import Home from "./Home";
+import MainPage from "./MainPage";
+import Calendar from './Calendar';
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="enterProfRot">
+          <Router>
+            <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/MainPage" element={<MainPage/>}/>
+            <Route path="/Calendar" element={<Calendar/>}/>
+            </Routes>
+          </Router>
+      </div>
     </div>
   );
 }
